f0d388a3ea55ccc4b0009c673cd91349ba914db4
TrunksLegendary <trunks0408@gmail.com>
2017-12-18 14:47:01 -0500

    Add files via upload

--- Shiny_VolcanicVEI/server.R
+++ Shiny_VolcanicVEI/server.R
@@ -2,10 +2,21 @@ shinyServer(function(input, output) {
 
   color <- colorFactor(topo.colors(7), volmap$VEI)
   
+  vol_vei <- reactive ({volmap %>% filter(VEI >= input$rangeInput[1] & VEI <= input$rangeInput[2])})
+  
+  factpal <- reactive ({
+    colorFactor(heat.colors(7),volmap$VEI, reverse = TRUE) })
+  
   output$map <- renderLeaflet({
-    map <- leaflet(volmap) %>%
-      setView(lng = -4.520604, lat = 23.209818, zoom = 2) %>%
-      addProviderTiles("CartoDB.Positron", options = providerTileOptions(noWrap = TRUE)) %>%
+    leaflet(volmap) %>%
+      setView(lng = -4.520604, lat = 23.209818, zoom = 1) 
+  })
+  
+  observe ({
+    pal <- factpal()
+    leafletProxy("map", data = vol_vei()) %>%
+      clearMarkers() %>% clearControls() %>%
+
       addCircleMarkers(
         lng=~longitude,
         lat=~latitude,
@@ -14,16 +25,21 @@ shinyServer(function(input, output) {
         color=~color(VEI),
         popup=~paste(
           "Year: ", as.character(year), "<br/>",
-          "Region: ", as.character(Region), "<br/>",
-          "VEI: ", as.character(VEI), "<br/>"
-        )
-      ) %>%
-      addLegend(
-        "bottomleft", # Legend position
-        pal=color, # color palette
-        values=~VEI, # legend values
-        opacity = 1,
-        title="VEI"
-      )
-  })
+         "Region: ", as.character(Region), "<br/>",
+          "VEI: ", as.character(VEI), "<br/>" ) 
+     ) %>%
+      addLegend(pal=color, values=~VEI, opacity = 1, title="VEI") %>%
+      #addLegend(title = "Magnitude", pal = pal, values = ~factor) %>%
+
+     fitBounds(~min(longitude), ~min(latitude), ~max(longitude), ~max(latitude))
+  })  
+  
+observe({
+  proxy <- leafletProxy("map", data = volmap)
+    proxy %>% clearTiles() %>%
+#      addProviderTiles(OpenStreetMap)
+      addProviderTiles(providers$Esri.WorldStreetMap)
+    
+})
+
 })
